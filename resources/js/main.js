38677fb178d2476b5932fd6ba4e2dad18b3a3c0f
njiang747 <nj3@princeton.edu>
2016-07-31 15:33:34 -0400

    attempt to fix slide transition (#2)

--- resources/js/main.js
+++ resources/js/main.js
@@ -44,6 +44,7 @@ function setup() {
     if (window.innerWidth <= 813) {
         var bg = document.getElementById("bg");
         bg.style.height = (bg.offsetHeight + 60) + "px";
+        bg.style.width = bg.offsetWidth + "px";
     }
 
     open_page("home_page");
