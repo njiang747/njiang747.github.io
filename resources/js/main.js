b39ed945ecb88e580cfa7a7c257fe34b1b780f77
njiang747 <nj3@princeton.edu>
2016-07-31 14:07:34 -0400

    added support for arrow key navigationand swipe navigation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,9 @@
 var cur_page;
+var cur_page_num = 0;
 
 window.onload = function() {
+    hotkey_setup();
+    hammer_setup();
     setup();
 }
 
@@ -18,6 +21,7 @@ function setup() {
             open_page(page_name);
         })
     }
+
     document.getElementById("nav_bar_mobile_button").addEventListener("click", function(e) {
         var nav_bar_inner = document.getElementById("nav_bar_inner");
         if (nav_bar_inner.offsetHeight == 0) {
@@ -32,22 +36,52 @@ function setup() {
             }, 500);
         }
     });
+
     document.getElementById("nav_bar_title").addEventListener("click", function(e) {
         open_page("home_page");
     })
+
     if (window.innerWidth <= 813) {
         var bg = document.getElementById("bg");
         bg.style.height = (bg.offsetHeight + 60) + "px";
-        // document.getElementById("nav_bar_title").innerHTML = (bg.offsetHeight + 60) + "px";
     }
+
     open_page("home_page");
 }
 
-function open_page(page_name) {
+function hotkey_setup() {
+    document.body.addEventListener("keyup", function(e) {
+        if (e.keyCode == 37) {
+            open_page(cur_page_num - 1);
+        } else if (e.keyCode == 39) {
+            open_page(cur_page_num + 1);
+        }
+    });
+}
+
+function hammer_setup() {
+    var hammertime = new Hammer(document.body);
+    hammertime.on('swipeleft', function(e) {
+        open_page(cur_page_num - 1);
+    });
+    hammertime.on('swiperight', function(e) {
+        open_page(cur_page_num + 1);
+    });
+}
+
+function open_page(page_val) {
+    var page_name = page_val;
+    var pages = document.getElementsByClassName("page");
+
+    if (!isNaN(page_val)) {
+        if (page_val < 0 || page_val >= 5) {
+            return;
+        }
+        page_name = pages[page_val].id;
+    }
     if (cur_page == page_name) {
         return;
     }
-    cur_page = page_name;
 
     if (page_name == "home_page") {
         document.getElementById("nav_bar").classList.remove("mini-nav");
@@ -55,10 +89,11 @@ function open_page(page_name) {
         document.getElementById("nav_bar").classList.add("mini-nav");
     }
 
-    var pages = document.getElementsByClassName("page");
     var page = document.getElementById(page_name);
-    var index = page.getAttribute("index");
+    var index = parseInt(page.getAttribute("index"));
     var nav_bar_items = document.getElementsByClassName("nav_bar_item");
+    cur_page = page_name;
+    cur_page_num = index;
 
     for (var i = 0; i < pages.length; i++) {
         if (window.innerWidth > 813) {
@@ -71,9 +106,11 @@ function open_page(page_name) {
             pages[i].classList.add("transparent");
         }
     }
+
     for (var i = 0; i < nav_bar_items.length; i++) {
         nav_bar_items[i].classList.remove("selected");
     }
+
     setTimeout(function() {
         for (var i = 0; i < pages.length; i++) {
             if (index != i) {
@@ -87,7 +124,12 @@ function open_page(page_name) {
                 }
             }
         }
+
+        for (var i = 0; i < nav_bar_items.length; i++) {
+            nav_bar_items[i].classList.remove("selected");
+        }
         document.getElementById(page_name + "_nav_item").classList.add("selected");
+
         page.classList.remove("display_none");
         setTimeout(function() {
             page.style.transform = "";
